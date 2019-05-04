4c0df5abaf67db400f5919df96aacef006f5a453
jessicatais <50267265+jessicatais@users.noreply.github.com>
2019-05-04 10:36:11 -0300

    Add files via upload

---
+++ desafio.rb
@@ -0,0 +1,26 @@
+puts "bem vindo ao jogo de adivinhacao"
+puts "qual e o seu nome?"
+nome = gets
+
+puts "comecaremos um jogo para voce " + nome
+puts
+puts
+puts
+puts
+puts "escolhemos um numero secreto entre 0 e 200..."
+numero_secreto=175
+puts"escolhido... que tal adivinhar hoje nosso numero secreto?"
+puts
+puts
+puts
+puts
+puts "tentativa 1 "
+puts "entre com o numero"
+chute=gets
+puts "sera que acertou? você chutou "+ chute 
+puts numero_secreto== chute.to_i
+
+
+
+
+
